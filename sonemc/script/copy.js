9a24508b9923d9260660a827d9fc76717780038a
Wejkey <rewejkem@gmail.com>
2025-04-16 20:15:54 +0200

    Update copy.js

--- sonemc/script/copy.js
+++ sonemc/script/copy.js
@@ -1,31 +1,28 @@
 function copyToClipboard() {
-    const codeBlock = document.querySelector('.plugin-config pre');
-    const range = document.createRange();
-    range.selectNodeContents(codeBlock);
-    const selection = window.getSelection();
-    selection.removeAllRanges();
-    selection.addRange(range);
-    
-    try {
-      document.execCommand('copy');
-      showTooltip();
-    } catch (err) {
-      console.error('Unable to copy', err);
-    }
+  const codeBlock = document.querySelector('.plugin-config pre');
+  const range = document.createRange();
+  range.selectNodeContents(codeBlock);
+  const selection = window.getSelection();
+  selection.removeAllRanges();
+  selection.addRange(range);
   
-    // Deselect after copy
-    selection.removeAllRanges();
+  try {
+    document.execCommand('copy');
+    showTooltip();
+  } catch (err) {
+    console.error('Unable to copy', err);
   }
-  
-  function showTooltip() {
-    const tooltip = document.querySelector('.tooltip');
-    tooltip.style.visibility = 'visible';
-    tooltip.style.opacity = '1';
-  
-    // Hide tooltip after 2 seconds
-    setTimeout(() => {
-      tooltip.style.visibility = 'hidden';
-      tooltip.style.opacity = '0';
-    }, 2000);
-  }
-  
+
+  selection.removeAllRanges();
+}
+
+function showTooltip() {
+  const tooltip = document.querySelector('.tooltip');
+  tooltip.style.visibility = 'visible';
+  tooltip.style.opacity = '1';
+
+  setTimeout(() => {
+    tooltip.style.visibility = 'hidden';
+    tooltip.style.opacity = '0';
+  }, 2000);
+}
